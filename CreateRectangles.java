430a2d8be1f43fa55da8faa1fbd2e9fc367f8c33
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2026-03-11 03:33:09 -0400

    fixed perimeter math

--- CreateRectangles.java
+++ CreateRectangles.java
@@ -20,7 +20,7 @@ class Rectangle {
     }
 
     public double getPerimeter() {
-        return this.height + this.width;
+        return 2 * (this.height + this.width);
     }
 }
 
@@ -31,12 +31,12 @@ class CreateRectangles {
         Rectangle rectangle2 = new Rectangle(3.5, 35.7);
 
         System.out.println(
-            "This is rectangle 1. It has a width of 4 and a height of 40. It's area is "
-            + rectangle1.getArea() + ", and it's perimeter is " + rectangle1.getPerimeter() + ".");
+            "This is rectangle 1. It has a width of 4 and a height of 40. Its area is "
+            + rectangle1.getArea() + ", and its perimeter is " + rectangle1.getPerimeter() + ".");
 
         System.out.println(
-            "This is rectangle 2. It has a width of 3.5 and a height of 35.7. It's area is "
-            + rectangle2.getArea() + ", and it's perimeter is " + rectangle2.getPerimeter() + ".");
+            "This is rectangle 2. It has a width of 3.5 and a height of 35.7. Its area is "
+            + rectangle2.getArea() + ", and its perimeter is " + rectangle2.getPerimeter() + ".");
     }
 }
 
